ec17c20e502efbed9d087a3a2d2de6784ee9edaa
souf <arhlabi.soufiane02@gmail.com>
2025-10-05 18:41:32 +0800

    create new uuid in auth srv

--- authentification/src/routes/new.ts
+++ authentification/src/routes/new.ts
@@ -1,8 +1,12 @@
 import express, {Request, Response} from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-router.post("/api/device", (req:Request,res:Response)=>{
+router.post("/api/device",
+      async (req:Request,res:Response)=>{
+      const deviceId = uuidv4();
+      res.status(200).send({uuid:deviceId})
 
 })
 
